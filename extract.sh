8c4772b5b301ca3abbc50798c432709696d2eced
Tom Gustafsson <tom.gustafsson@iki.fi>
2020-05-19 13:43:47 +0300

    Run 5 adaptive refinements instead of 4

--- extract.sh
+++ extract.sh
@@ -6,6 +6,7 @@ jq -r '.cells[3].outputs[1].data."image/png"' nitsche_corners.nbconvert.ipynb | 
 jq -r '.cells[3].outputs[3].data."image/png"' nitsche_corners.nbconvert.ipynb | base64 -d > ex2_adaptive_2.png
 jq -r '.cells[3].outputs[5].data."image/png"' nitsche_corners.nbconvert.ipynb | base64 -d > ex2_adaptive_3.png
 jq -r '.cells[3].outputs[7].data."image/png"' nitsche_corners.nbconvert.ipynb | base64 -d > ex2_adaptive_4.png
+jq -r '.cells[3].outputs[9].data."image/png"' nitsche_corners.nbconvert.ipynb | base64 -d > ex2_adaptive_5.png
 jq -r '.cells[4].outputs[1].data."image/png"' nitsche_corners.nbconvert.ipynb | base64 -d > ex2_uniform_1.png
 jq -r '.cells[4].outputs[3].data."image/png"' nitsche_corners.nbconvert.ipynb | base64 -d > ex2_uniform_2.png
 jq -r '.cells[4].outputs[5].data."image/png"' nitsche_corners.nbconvert.ipynb | base64 -d > ex2_uniform_3.png
